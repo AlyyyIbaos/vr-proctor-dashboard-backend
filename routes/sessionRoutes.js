7c31f693b316109768f4ec5fdcfb2f5ad52d89a4
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-24 02:29:26 +0800

    fixing authmid

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import supabase from "../config/supabaseClient.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
