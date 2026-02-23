7442d113891337bf7dff91b847a206eed8925733
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-24 01:13:27 +0800

    server cors

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ import alertSocket from "./sockets/alertSocket.js";
 // APP SETUP
 // ==============================
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
