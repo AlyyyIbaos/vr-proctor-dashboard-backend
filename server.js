77b86f809c50ce964c2bcf3521aa889a79016b31
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-24 10:23:45 +0800

    server listen port not hard coded

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get("/", (req, res) => {
 // ==============================
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
 });
