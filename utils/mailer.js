b8742d75e82b1627b6711adb94535cde97ad437d
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-25 00:57:21 +0800

    resend email

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,11 +4,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendOTPEmail(toEmail, otp) {
   await resend.emails.send({
-    from: "SynapSee <onboarding@resend.dev>",
+    from: "SynapSee <alyssadeniseibaos@gmail.com>",
     to: toEmail,
     subject: "Your SynapSee OTP Code",
     html: `
-      <h2>SynapSee VR Proctor</h2>
+      <h2>SynapSee VR Proctor System</h2>
       <p>Your One-Time Password (OTP) is:</p>
       <h1 style="letter-spacing:4px;">${otp}</h1>
       <p>This code will expire in 5 minutes.</p>
