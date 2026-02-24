53e24dc717537fbc2bfbebdd9de64efac189fa49
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-24 09:40:55 +0800

    origin: true (cors)

--- server.js
+++ server.js
@@ -33,15 +33,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow Postman
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true, // reflect request origin automatically
     credentials: true,
   }),
 );
